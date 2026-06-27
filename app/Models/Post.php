105b46cf3cb8990b5907df86f3d3ab884ae383e0
lucasdvillegas <lucasdvillegas@hotmail.com>
2026-06-27 01:51:08 -0300

    tests fixed 12

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,17 +2,13 @@
 
 namespace App\Models;
 
+use Database\Factories\PostFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * @template TFactory of \Database\Factories\PostFactory
- * @use HasFactory<TFactory>
- *
- * @mixin \Eloquent
- */
 class Post extends Model
 {
+     /** @use HasFactory<PostFactory> */
     use HasFactory;
 
     protected $fillable = [
